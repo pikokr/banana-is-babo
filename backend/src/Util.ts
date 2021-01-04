cd13e79169375918b972d55e98e1187ff6a93af4
pikokr <parangee9706@gmail.com>
2021-01-04 21:29:02 +0900

    feat: login

---
+++ backend/src/Util.ts
@@ -0,0 +1,18 @@
+import fetch, {RequestInfo, RequestInit, Response} from "node-fetch";
+
+export default class Util {
+    static async safeFetch(info: RequestInfo, init: RequestInit): Promise<Response> {
+        return new Promise(async (resolve, reject) => {
+            let rejected = false
+            const data = await fetch(info, init).catch(() => {
+                rejected = true
+                return reject()
+            }) as Response
+            if (rejected) return
+            if (data.status === 429) { // @ts-ignore
+                return new Promise(async resolve1 => setTimeout(await data.json().then(r=>r.retry_after), resolve1)).then(() => resolve(this.safeFetch(info, init)))
+            }
+            resolve(data)
+        })
+    }
+}
--- backend/src/resolvers/Mutation/index.ts
+++ backend/src/resolvers/Mutation/index.ts
@@ -4,6 +4,7 @@ import config from '../../../config.json'
 import fetch from "node-fetch";
 import {DISCORD_API_ENDPOINT} from "../../constants";
 import jwt from 'jsonwebtoken'
+import Util from "../../Util";
 
 export default {
     login: async (source, {code}: {code: string}, ctx) => {
@@ -12,24 +13,27 @@ export default {
             client_id: config.oauth2.clientID,
             code,
             client_secret: config.oauth2.clientSecret,
-            redirect_uri: config.oauth2.redirect_uri,
-            grant_type: 'authorization_code'
+            redirect_uri: config.oauth2.callbackURI,
+            grant_type: 'authorization_code',
+            scope: 'identify'
         })
         const res = await fetch(DISCORD_API_ENDPOINT + '/oauth2/token', {
             body: params,
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded'
-            }
+            },
+            method: 'POST'
         })
-        if (res.status !== 200) return null
         const json = await res.json()
+        if (res.status !== 200) return null
         if (!json.access_token) return null
-        const user = await fetch(DISCORD_API_ENDPOINT + '/users/@me', {
+        const user = await Util.safeFetch(DISCORD_API_ENDPOINT + '/users/@me', {
             headers: {
                 Authorization: `${json.token_type} ${json.access_token}`
             }
         })
+        const json2 = await user.json()
         if (user.status !== 200) return null
-        return jwt.sign(await user.json(), config.jwtSecret)
+        return jwt.sign(json2, config.jwtSecret)
     }
 } as IResolvers
