00dd589050d758e04de695ef3fca8558ecf2ca40
pikokr <parangee9706@gmail.com>
2021-01-04 22:29:26 +0900

    feat: process login

--- frontend/src/views/LoginCallback.tsx
+++ frontend/src/views/LoginCallback.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import Apollo from "../Apollo";
+import {gql} from "@apollo/client/core";
 
 const LoginCallback = () => {
+    const [initial, setInitial] = React.useState(true)
+    const params = new URLSearchParams(window.location.search)
+    const code = params.get('code')
+    if (!code) {
+        window.location.assign('/')
+        return null
+    }
+
+    if (initial) {
+        setInitial(false);
+        (async () => {
+            const {data} = await Apollo.mutate({
+                mutation: gql`
+                mutation($code: String!) {
+                    login(code: $code)
+                }
+                `,
+                variables: {
+                    code
+                }
+            })
+            if (data?.login) {
+                localStorage.setItem('token', data.login)
+            }
+            window.location.assign('/')
+        })()
+    }
     return (
         <div className="text-center pt-10">
             <FontAwesomeIcon size="9x" icon={['fab', 'discord']}/>
