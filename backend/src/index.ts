09f99652d47f76c71a5511edd7e5214372e724da
pikokr <parangee9706@gmail.com>
2021-01-04 19:52:52 +0900

    feat: setup basic apollo graphql server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello world!')
+import 'graphql-import-node'
+import {ApolloServer} from "apollo-server";
+import schema from '../schema.graphql'
+import resolvers from './resolvers'
+// @ts-ignore
+import config from '../config.json'
+
+const server = new ApolloServer({
+    typeDefs: schema,
+    resolvers: resolvers as any
+})
+
+server.listen(config.port, () => console.log('Listening'))
---
+++ backend/src/resolvers/index.ts
@@ -0,0 +1,6 @@
+import {IResolvers} from "graphql-tools";
+
+export default {
+} as {
+    [key: string]: IResolvers|undefined
+}
