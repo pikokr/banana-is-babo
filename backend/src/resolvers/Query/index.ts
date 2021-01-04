e8373cabb33f9ee0c2902a2f2ee72acc5bec7a02
pikokr <parangee9706@gmail.com>
2021-01-04 21:45:04 +0900

    feat: login link query

--- backend/src/resolvers/Query/index.ts
+++ backend/src/resolvers/Query/index.ts
@@ -1,5 +1,9 @@
 import {IResolvers} from "graphql-tools";
+// @ts-ignore
+import config from '../../../config.json'
+import {DISCORD_API_ENDPOINT} from "../../constants";
 
 export default {
-    me: (source, args, context) => context.user || null
+    me: (source, args, context) => context.user || null,
+    login: () => `${DISCORD_API_ENDPOINT}/oauth2/authorize?client_id=${config.oauth2.clientID}&redirect_uri=${encodeURIComponent(config.oauth2.callbackURI)}&response_type=code&scope=identify`
 } as IResolvers
